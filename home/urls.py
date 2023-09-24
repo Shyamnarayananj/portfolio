f6cc878cc14762f8fab5aa108b46da563ff95715
J SHYAM NARAYANAN <145639384+Shyamnarayananj@users.noreply.github.com>
2023-09-24 20:55:07 +0530

    Add files via upload
    my personal portfolio site under construction.

---
+++ home/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from home import views
+
+
+# Django admin header customization
+
+admin.site.site_header="Login to Developer shyam"
+admin.site.site_title="Welcome to Shyam's Dashboard"
+admin.site.index_title="Welcome to this portal"
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
+]
